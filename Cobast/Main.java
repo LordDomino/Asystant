d5ae5d1cd6b7b1debd49633238733e1aff0c10e2
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2022-11-25 20:06:43 +0800

    Changes to be committed: 	modified:   Cobast/Main.java

--- Cobast/Main.java
+++ Cobast/Main.java
@@ -66,6 +66,7 @@ public class Main {
                     case 3:
                         break;
                     case 4:
+                        System.out.println("Wow");
                         break;
                 }
             }
