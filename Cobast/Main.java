bbcc0a1c28deedbd36d7469055cd64f706cfc463
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2022-11-25 17:25:55 +0800

    Changes to be committed: 	modified:   Cobast/Main.java

--- Cobast/Main.java
+++ Cobast/Main.java
@@ -1,18 +1,20 @@
 package cobast;
 
-import cobast.input.Image;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        scan_image(new Image("Hello world!"));
-    }
-    public static void scan_image(Image image) {
-        System.out.println(image);
-        System.out.println("woa");    
-        System.out.println("Hello world");
-        System.out.println("woa2");    
-        System.out.println("Hello world2");
-        System.out.println("woa3");    
-        System.out.println("Hello world3");
+        
+        boolean console_mainloop = true;
+        
+        // Menu variables
+        String 
+
+        while (console_mainloop == true) {
+            // Begin initial printing for menu
+
+
+            System.out.println("");
+        }
     }
 }
