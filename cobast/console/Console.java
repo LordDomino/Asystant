f620378bb6895d2022aada95a09f379d195cbcd9
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2022-11-29 19:23:45 +0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   cobast/console/Console.java

--- cobast/console/Console.java
+++ cobast/console/Console.java
@@ -4,132 +4,130 @@ package cobast.console;
  * Class for console-related operations.
  */
 public class Console {
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_BOLD = "\u001b[1m";
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-    public static final String ANSI_BOLD = "\u001b[1m";
+	/* All private, usually finals, variables below. These are the base defaults
+	 * that are internally hard-coded in the program. Hence, these variables are
+	 * specified as "private" variables.
+	*/
 
-    /* All private, usually finals, variables below. These are the base defaults
-     * that are internally hard-coded in the program. Hence, these variables are
-     * specified as "private" variables.
-    */
+	/** Default console length; is static and immutable */
+	private static final int _def_console_length = 50;
 
-    /** Default console length; is static and immutable */
-    private static final int _def_console_length = 50;
+	/** Default header length which is the same length as the default console length */
+	private static final String _def_header = new String(new char[_def_console_length]).replace("\0", "=");
 
-    /** Default header length which is the same length as the default console length */
-    private static final String _def_header = new String(new char[_def_console_length]).replace("\0", "=");
-    
-    /* All public, mutable variables below */
+	/* All public, mutable variables below */
 
-    public static int console_length = _def_console_length;
-    public static String header = _def_header;
-    /** Boolean that determines running state of the program. */
-    public static boolean mainloop = false;
-    /** Boolean that prevents screen state from reloading. Use this for screens
-     * that require reloading and screen updates.
-    */
-    public static boolean mainhold = true;
-    /** Boolean that indicates invalidity in program. This can be used as a flag
-     * to trigger warnings and errors.
-    */
-    public static boolean invalid = false;
+	public static int console_length = _def_console_length;
+	public static String header = _def_header;
+	/** Boolean that determines running state of the program. */
+	public static boolean mainloop = false;
+	/** Boolean that prevents screen state from reloading. Use this for screens
+	 * that require reloading and screen updates.
+	*/
+	public static boolean mainhold = true;
+	/** Boolean that indicates invalidity in program. This can be used as a flag
+	 * to trigger warnings and errors.
+	*/
+	public static boolean invalid = false;
 
-    public Console(String console_name) {}
+	public Console(String console_name) {}
 
-    public static void main(String[] args) {
-        System.out.println(header);
-    }
+	public static void main(String[] args) {
+		System.out.println(header);
+	}
 
-    /** Set the console length.
-     * @param length The length of the console interface; default length is 50
-     */
-    public static void setConsoleLength(int length) {
-        console_length = length;
-    }
+	/** Set the console length.
+	 * @param length The length of the console interface; default length is 50
+	 */
+	public static void setConsoleLength(int length) {
+		console_length = length;
+	}
 
-    /** Creates a string of header. 
-     * @param decor The decoration character to be used as the header border
-     * @param text Title text of the header
-     * @param text_indent Amount of characters to indent the text. Use 0 for
-     * no idention
-    */
-    public static void setHeader(char decor, String text, int text_indent) {
-        // create string of decor chars with length based on console length
-        String temp_header = new String(new char[console_length]).replace("\0", String.valueOf(decor));
+	/** Creates a string of header.
+	 * @param decor The decoration character to be used as the header border
+	 * @param text Title text of the header
+	 * @param text_indent Amount of characters to indent the text. Use 0 for
+	 * no idention
+	*/
+	public static void setHeader(char decor, String text, int text_indent) {
+		// create string of decor chars with length based on console length
+		String temp_header = new String(new char[console_length]).replace("\0", String.valueOf(decor));
 
-        // checks if text indention is beyond the length of header
-        if (text_indent >= temp_header.length()) {
-            text_indent = temp_header.length() - 1;
-        }
+		// checks if text indention is beyond the length of header
+		if (text_indent >= temp_header.length()) {
+			text_indent = temp_header.length() - 1;
+		}
 
-        if (text.length() >= temp_header.length()) {
-            header = text;
-        } else {
-            header = temp_header.substring(0, text_indent)
-                + text
-                + temp_header.substring(text_indent + text.length() - 1);
-        }
-    }
+		if (text.length() >= temp_header.length()) {
+			header = text;
+		} else {
+			header = temp_header.substring(0, text_indent)
+						+ text
+						+ temp_header.substring(text_indent + text.length() - 1);
+		}
+	}
 
-    /** @return Returns console length. If no console length is set using the
-     * {@code setConsoleLength()}, the default console length is 50.
-     */
-    public static int getConsoleLength() {
-        return console_length;
-    }
+	/** @return Returns console length. If no console length is set using the
+	 * {@code setConsoleLength()}, the default console length is 50.
+	 */
+	public static int getConsoleLength() {
+		return console_length;
+	}
 
-    /** @return Returns header string. If no header string is set using the
-     * {@code setHeader()}, the default header string is a string of fifty '='.
-     */
-    public static String getHeader() {
-        return header;
-    }
+	/** @return Returns header string. If no header string is set using the
+	 * {@code setHeader()}, the default header string is a string of fifty '='.
+	 */
+	public static String getHeader() {
+		return header;
+	}
 
-    /** Clears the console screen. */
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
+	/** Clears the console screen. */
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 
-    /** Starts the mainloop of the console. Synonymous to "running" the program. */
-    public static void startloop() {
-        mainloop = true;
-    }
+	/** Starts the mainloop of the console. Synonymous to "running" the program. */
+	public static void startloop() {
+		mainloop = true;
+	}
 
-    public static void stoploop() {
-        mainloop = false;
-    }
+	public static void stoploop() {
+		mainloop = false;
+	}
 
-    /** Updates console state to defaults. Works with {@code collapse()} when
-     * attempting to reload the program.
-     */
-    public static void update() {
-        mainhold = true;
-        invalid = false;
-    }
+	/** Updates console state to defaults. Works with {@code collapse()} when
+	 * attempting to reload the program.
+	 */
+	public static void update() {
+		mainhold = true;
+		invalid = false;
+	}
 
-    /** Collapses loop to allow reloading. */
-    public static void collapse() {
-        mainhold = false;
-    }
+	/** Collapses loop to allow reloading. */
+	public static void collapse() {
+		mainhold = false;
+	}
 
-    public static void reassure() {
-        invalid = false;
-    }
+	public static void reassure() {
+		invalid = false;
+	}
 
-    /** Flags boolean {@code invalid} as true, allowing error triggers and
-     * warning flags.
-    */
-    public static void invalidate() {
-        invalid = true;
-    }
-
+	/** Flags boolean {@code invalid} as true, allowing error triggers and
+	 * warning flags.
+	*/
+	public static void invalidate() {
+		invalid = true;
+	}
 }
