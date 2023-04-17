3dd637dc12e4d4f81cb4c66564a427ef56ef5d81
LordDomino <louidominicnaquita16@gmail.com>
2023-04-17 22:58:58 +0800

    Optimized Row class

--- projectfiles/main.java
+++ projectfiles/main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String args[]) {
         Row testRow = new Row();
         String[] fields1 = {"(Last name)", "(First name)", "(Middle initial)"};
-        testRow.createPartition(fields1);
+        testRow.createPartition(fields1, "Name: ");
         String[] fields2 = {"Level and Section"};
-        testRow.createPartition(fields2);
+        testRow.createPartition(fields2, "");
         testRow.wrap();
     }
 }
--- projectfiles/structures/Row.java
+++ projectfiles/structures/Row.java
@@ -1,35 +1,54 @@
+/*
+ * Written by Loui Dominic Naquita
+ */
+
 package projectfiles.structures;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.FlowLayout;
 import javax.swing.border.MatteBorder;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Row {
 
-    JFrame mainframe;
+    JFrame mainframe; // create mainframe for future instance
 
     public Row() {
-        mainframe = new JFrame();
-        mainframe.setSize(300, 100);
-        mainframe.setLayout(new FlowLayout(FlowLayout.LEFT));
+        mainframe = new JFrame(); // instantiate JFrame class
+        mainframe.setSize(500, 100);
+        mainframe.setLayout(new FlowLayout(FlowLayout.LEFT)); // set layout as FlowLayout to align left to right
+        mainframe.pack();
+        mainframe.setResizable(false);
     }
 
-    public void createPartition(String fields[]) {
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        MatteBorder separator = new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0));
+    public void createPartition(String fields[], String label) {
+        Font defaulFont = new Font("Arial", 0, 18);
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // container panel for all fields
+        MatteBorder border = new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)); // border for the panel
         
+        if (label != "") {
+            JTextArea panelLabel = new JTextArea(label);
+            panelLabel.setFont(defaulFont);
+            panelLabel.setLineWrap(true);
+            panelLabel.setEditable(false);
+            panel.add(panelLabel);
+        } else {}
 
         for(String field : fields) {
-            JLabel fieldLabel = new JLabel(field);
+            JTextArea fieldLabel = new JTextArea(field);
+            // fieldLabel.setLineWrap(true);
+            fieldLabel.setEditable(false);
+            fieldLabel.setFont(defaulFont);
             panel.add(fieldLabel);
         }
 
         panel.setSize(300, 100);
-        panel.setBorder(separator);
+        panel.setBorder(border);
         mainframe.add(panel);
+        mainframe.pack();
     }
 
     public void wrap() {
