2596ace7c858274b4c372a2994172acdbd2c843d
LordDomino <louidominicnaquita16@gmail.com>
2023-04-17 22:59:55 +0800

    Removed form section folder

--- projectfiles/formSecA/Test.java
+++
@@ -1,9 +0,0 @@
-package projectfiles.formSecA;
-
-import projectfiles.formSecA.name;
-
-public class Test {
-    public static void main(String[] args) {
-        name f_name = new name();
-    }
-}
