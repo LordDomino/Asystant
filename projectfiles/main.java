180d4e06b5adae8d976d24f7bde0ea5c35e93369
LordDomino <louidominicnaquita16@gmail.com>
2023-04-17 23:03:38 +0800

    Added Section A

--- projectfiles/main.java
+++ projectfiles/main.java
@@ -1,14 +1,9 @@
 package projectfiles;
 
-import projectfiles.structures.Row;
+import projectfiles.SectionA;
 
 public class Main {
     public static void main(String args[]) {
-        Row testRow = new Row();
-        String[] fields1 = {"(Last name)", "(First name)", "(Middle initial)"};
-        testRow.createPartition(fields1, "Name: ");
-        String[] fields2 = {"Level and Section"};
-        testRow.createPartition(fields2, "");
-        testRow.wrap();
+        SectionA secA = new SectionA();
     }
 }
