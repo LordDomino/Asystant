bd8754558316e6ff9424bb846e3d7094523c1a81
LordDomino <louidominicnaquita16@gmail.com>
2023-04-19 02:18:37 +0800

    Added Javadocs

--- projectfiles/Utils.java
+++ projectfiles/Utils.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Utils {
+    /**
+     * Creates an ArrayList directly from a String[] array object.
+     */
     public static ArrayList<String> newArray(String[] array) {
         return new ArrayList<String>(Arrays.asList(array));
     }
