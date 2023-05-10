70fd5d4ec53184c048e9f8023cd82b5a54e80d64
LordDomino <louidominicnaquita16@gmail.com>
2023-05-10 22:33:56 +0800

    Cleaned the form sections

--- projectfiles/GUI/formSections/SectionA.java
+++ projectfiles/GUI/formSections/SectionA.java
@@ -1,36 +1,10 @@
 package projectfiles.gui.formSections;
 
-import projectfiles.components.CRow;
-import projectfiles.components.Section;
+import projectfiles.components.CSection;
 
-/**
- * Written by Loui Dominic Naquita
- */
-public final class SectionA extends Section {
+public final class SectionA extends CSection {
 
     public SectionA() {
         super("A. PERSONAL INFORMATION");
-        CRow row1 = new CRow();
-        CRow row2 = new CRow();
-        CRow row3 = new CRow();
-        CRow row4 = new CRow();
-        CRow row5 = new CRow();
-        
-        row1.createFieldGroup("Name", new String[]{"(Last name)", "(First name)", "(Middle initial)"});
-        row1.createFieldGroup(new String[]{"Level/Section"});
-        row2.createFieldGroup(new String[]{"Address"});
-        row3.createFieldGroup("Contact Number", new String[]{"(Mobile / Landline number)"});
-        row3.createFieldGroup(new String[]{"Date of birth"});
-        row3.createFieldGroup(new String[]{"Age"});
-        row3.createFieldGroup(new String[]{"Height (in ft.)"});
-        row3.createFieldGroup(new String[]{"Weight (in kg.)"});
-        row4.createFieldGroup(new String[]{"Name of Mother", "Occupation", "Contact number:", "Email"});
-        row5.createFieldGroup(new String[]{"Name of Father", "Occupation", "Contact number:", "Email"});
-
-        this.add(row1);
-        this.add(row2);
-        this.add(row3);
-        this.add(row4);
-        this.add(row5);
     }
 }
--- projectfiles/GUI/formSections/SectionB.java
+++ projectfiles/GUI/formSections/SectionB.java
@@ -1,18 +1,9 @@
 package projectfiles.gui.formSections;
 
-import projectfiles.components.CRow;
-import projectfiles.components.Section;
+import projectfiles.components.CSection;
 
-public class SectionB extends Section {
+public class SectionB extends CSection {
     public SectionB() {
         super("B. CONTACT PERSON IN CASE OF EMERGENCY");
-
-        CRow row1 = new CRow();
-        
-        row1.createFieldGroup("Name of contact person", new String[]{"Last name", "First name", "Middle initial"});
-        row1.createFieldGroup(new String[]{"Relationship"});
-        row1.createFieldGroup(new String[]{"Contact number"});
-
-        this.add(row1);
     }
 }
