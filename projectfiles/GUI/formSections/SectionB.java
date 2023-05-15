409dbc60aceb44e0fc09525a703a75f7f4626183
Potatopowers <118539229+Potatopowers@users.noreply.github.com>
2023-05-15 10:58:41 +0800

    created the missing sections
    created the missing sections

--- projectfiles/GUI/formSections/SectionB.java
+++ projectfiles/GUI/formSections/SectionB.java
@@ -1,9 +1,21 @@
 package projectfiles.gui.formSections;
 
+import projectfiles.components.CRow;
 import projectfiles.components.CSection;
+import projectfiles.components.CTextFieldGroup;
 
 public class SectionB extends CSection {
-	public SectionB() {
+	public SectionB() throws Exception {
 		super("B. CONTACT PERSON IN CASE OF EMERGENCY");
+	
+		CTextFieldGroup r1_g1 = new CTextFieldGroup("Name of Contact Person:", new String[]{"Last name", "First name", "Middle Initial"});
+        CTextFieldGroup r1_g2 = new CTextFieldGroup(new String[]{"Relationship"});
+		CTextFieldGroup r1_g3 = new CTextFieldGroup(new String[]{"Contact No:"});
+
+		CRow row1 = new CRow(r1_g1, r1_g2, r1_g3);
+
+		this.add(row1);
+
 	}
+
 }
--- projectfiles/GUI/formSections/SectionC.java
+++ projectfiles/GUI/formSections/SectionC.java
@@ -3,15 +3,17 @@ package projectfiles.gui.formSections;
 import javax.swing.JSeparator;
 
 import projectfiles.components.CDetailedCheckboxGroup;
+import projectfiles.components.CDetailedFormField;
 import projectfiles.components.CDetailedMultipleChoice;
 import projectfiles.components.CFieldGroup;
 import projectfiles.components.CSection;
 import projectfiles.components.CSeparator;
+import projectfiles.components.CTextFieldGroup;
 
 public class SectionC extends CSection {
     public SectionC() throws Exception {
         super("C. HEALTH AND SAFETY CONDITIONS");
-
+  
         CDetailedMultipleChoice group1 = new CDetailedMultipleChoice(
             "1. Is your child subject to seizures, fainting, epilepsy, bleeding, asthma, or any other condition that may affect his or her safety?",
             CFieldGroup.LEFT,
@@ -33,4 +35,7 @@ public class SectionC extends CSection {
         this.add(sep1);
         this.add(group2);
     }
+
 }
+
+
